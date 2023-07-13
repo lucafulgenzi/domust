d4191c7778fdf8962f6e04f98a1de2c1f69131a5
zappaboy <federico.zappone@justanother.cloud>
2023-07-13 13:04:10 +0200

    Completed switchbot devices addition;

--- src/devices/switchbot.rs
+++ src/devices/switchbot.rs
@@ -3,13 +3,10 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 use base64::engine::general_purpose;
 use base64::Engine;
-use hmac::{Hmac, Mac};
-use reqwest::header::HeaderMap;
-use sha2::Sha256;
+use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE};
+use ring::hmac::{self, Key, HMAC_SHA256};
 
 use crate::core::{Config, Device};
-
-type HmacSha256 = Hmac<Sha256>;
 
 pub async fn exec_switchbot_command(config: &Config, device: &Device, command: String) {
     log::debug!(
@@ -33,37 +30,48 @@ pub async fn exec_switchbot_command(config: &Config, device: &Device, command: S
         .as_millis()
         .to_string();
 
+    log::debug!("t: {}", t);
     let string_to_sign = token + &*t + &*nonce;
-    let key = string_to_sign.as_bytes();
-    let mut mac = HmacSha256::new_from_slice(key).unwrap();
-    mac.update(string_to_sign.as_ref());
-    let result = mac.finalize();
-    let result_bytes = result.into_bytes();
+    log::debug!("String to sign: {}", string_to_sign);
+    let bytes_to_sign = string_to_sign.as_bytes();
+
+    let result_bytes = sign_message(bytes_to_sign, &*config.switchbot.secret.as_bytes());
+
     let sign = general_purpose::STANDARD.encode(result_bytes);
+    log::debug!("Sign: {}", sign);
 
     let mut switchbot_headers = HeaderMap::new();
     switchbot_headers.insert(
-        "Content-type",
-        "application/json; charset=utf8".parse().unwrap(),
+        CONTENT_TYPE,
+        HeaderValue::from_str("application/json; charset=utf8").unwrap(),
     );
-    switchbot_headers.insert("Authorization", config.switchbot.token.parse().unwrap());
-    switchbot_headers.insert("sign", sign.parse().unwrap());
-    switchbot_headers.insert("t", t.parse().unwrap());
-    switchbot_headers.insert("nonce", nonce.parse().unwrap());
+    switchbot_headers.insert(
+        AUTHORIZATION,
+        HeaderValue::from_str(&*config.switchbot.token).unwrap(),
+    );
+    switchbot_headers.insert("sign", HeaderValue::from_str(&*sign).unwrap());
+    switchbot_headers.insert("t", HeaderValue::from_str(&*t).unwrap());
+    switchbot_headers.insert("nonce", HeaderValue::from_str(&*nonce).unwrap());
 
     let mut base_request_url: String = config.switchbot.base_url.to_string() + "/v1.1/devices/";
     base_request_url += &*device.switchbot_device_id;
     base_request_url.push_str("/commands");
-    reqwest::Client::new()
-        .post(&config.switchbot.base_url)
+
+    log::debug!("Switchbot request URL: {}", base_request_url);
+    log::debug!("Switchbot request headers: {:?}", switchbot_headers);
+    log::debug!("Switchbot request data: {:?}", data);
+
+    let res = reqwest::Client::new()
+        .post(base_request_url)
         .json(&data)
         .headers(switchbot_headers)
         .send()
         .await
         .unwrap_or_else(|e| {
-            log::error!("Error sending request to broadlink manager: {:?}", e);
+            log::error!("Error sending request to switchbot hub: {:?}", e);
             std::process::exit(1);
         });
+    log::debug!("Switchbot response: {:?}", res);
 }
 
 fn check_device_options(device: &Device) {
@@ -78,4 +86,10 @@ fn check_existence(value: String, error_message: &str) {
         log::error!("{}", error_message);
         std::process::exit(1);
     }
+}
+
+fn sign_message(message: &[u8], secret: &[u8]) -> Vec<u8> {
+    let key = Key::new(HMAC_SHA256, secret);
+    let signature = hmac::sign(&key, message);
+    signature.as_ref().to_vec()
 }
